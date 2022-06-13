b03ac237d98d7f470707845293e752e258a2e15d
Srutip04 <subuhishruti75@gmail.com>
2022-06-13 13:47:40 +0530

    reading planets data

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
-const { parse } = require("csv-parse");
-const fs = require("fs");
+// const { parse } = require("csv-parse");
+// const fs = require("fs");
 
-const habitablePlanets = [];
+// const habitablePlanets = [];
 
-function isHabitablePlanet(planet) {
-  return (
-    planet["koi_disposition"] === "CONFIRMED" &&
-    planet["koi_insol"] > 0.36 &&
-    planet["koi_insol"] < 1.11 &&
-    planet["koi_prad"] < 1.6
-  );
-}
+// function isHabitablePlanet(planet) {
+//   return (
+//     planet["koi_disposition"] === "CONFIRMED" &&
+//     planet["koi_insol"] > 0.36 &&
+//     planet["koi_insol"] < 1.11 &&
+//     planet["koi_prad"] < 1.6
+//   );
+// }
 
-fs.createReadStream("kepler_data.csv")
-  .pipe(
-    parse({
-      comment: "#",
-      columns: true,
-    })
-  )
-  .on("data", (data) => {
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
-    }
+// fs.createReadStream("kepler_data.csv")
+//   .pipe(
+//     parse({
+//       comment: "#",
+//       columns: true,
+//     })
+//   )
+//   .on("data", (data) => {
+//     if (isHabitablePlanet(data)) {
+//       habitablePlanets.push(data);
+//     }
+//   })
+//   .on("error", (err) => {
+//     console.log(err);
+//   })
+//   .on("end", () => {
+//     console.log(
+//       habitablePlanets.map((planet) => {
+//         return planet["kepler_name"];
+//       })
+//     );
+//     console.log(`${habitablePlanets.length} habitable planets found!`);
+//   });
+
+
+const {parse} = require('csv-parse');
+const fs = require('fs');
+
+const results =[]
+
+fs.createReadStream('kepler_data.csv')
+  .on('data',(data)=>{
+    results.push(data);
   })
-  .on("error", (err) => {
+  .on('error',(err)=>{
     console.log(err);
   })
-  .on("end", () => {
-    console.log(
-      habitablePlanets.map((planet) => {
-        return planet["kepler_name"];
-      })
-    );
-    console.log(`${habitablePlanets.length} habitable planets found!`);
-  });
+  .on('end',() =>{
+    console.log(results)
+    console.log('done');
+  })
